f193d179d56f76d88d2af37b4984e44ef48c964a
Jason Toups <jason.p.toups@gmail.com>
2018-04-19 13:24:50 -0400

    add deployment

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,10 @@ const connectionURI = `mongodb://localhost/2018candidates_db`
 
 mongoose.Promise = Promise
 
-mongoose.connect(connectionURI)
-  .then(console.log('db connection established'))
-  .catch(console.log('db connection failed'))
+if (process.env.NODE_ENV === "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect(connectionURI)
+}
 
 module.exports = mongoose
